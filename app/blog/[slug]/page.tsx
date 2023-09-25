43f4a269c8bff521c9ca9f67981566cb63896f0a
colineckert <colineckert10@gmail.com>
2023-09-24 19:30:30 -0700

    basic blog page

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Post {
+  title: string;
+  content: string;
+  slug: string;
+}
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function BlogPostPage({ params }: Props) {
+  const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+    (res) => res.json()
+  );
+  const post = posts.find((post) => post.slug === params.slug)!;
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
