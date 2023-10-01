a9276f6e60aa410cf735a9fba42724b18d15a430
colineckert <colineckert10@gmail.com>
2023-10-01 11:19:51 -0700

    create users pages

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,7 +3,5 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const users = await prisma.user.findMany();
-  console.log(users);
-
   return NextResponse.json(users);
 }
