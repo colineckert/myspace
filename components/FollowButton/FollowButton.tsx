fadacfc7b50c3e1f5abbf735c5bbc15f391c59a3
colineckert <colineckert10@gmail.com>
2023-10-01 11:20:07 -0700

    create follow button

---
+++ components/FollowButton/FollowButton.tsx
@@ -0,0 +1,24 @@
+import { getServerSession } from 'next-auth';
+import FollowClient from './FollowClient';
+import { prisma } from '@/lib/prisma';
+import { authOptions } from '../../app/api/auth/[...nextauth]/route';
+
+interface Props {
+  targetUserId: string;
+}
+
+export default async function FollowButton({ targetUserId }: Props) {
+  const session = await getServerSession(authOptions);
+
+  const currentUserId = await prisma.user
+    .findFirst({ where: { email: session?.user?.email! } })
+    .then((user) => user?.id!);
+
+  const isFollowing = await prisma.follows.findFirst({
+    where: { followerId: currentUserId, followingId: targetUserId },
+  });
+
+  return (
+    <FollowClient targetUserId={targetUserId} isFollowing={!!isFollowing} />
+  );
+}
