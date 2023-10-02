96ff883ca30cd2d09606ddb4411caf1437517eb9
colineckert <colineckert10@gmail.com>
2023-10-02 06:37:00 -0700

    add user update route

---
+++ app/api/user/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/lib/prisma';
+import { authOptions } from '../auth/[...nextauth]/route';
+
+export async function PUT(req: Request) {
+  const session = await getServerSession(authOptions);
+  const currentUserEmail = session?.user?.email!;
+
+  const data = await req.json();
+  data.age = Number(data.age);
+
+  const user = await prisma.user.update({
+    where: {
+      email: currentUserEmail,
+    },
+    data,
+  });
+
+  return NextResponse.json(user);
+}
--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-import React from 'react';
 import { useSession } from 'next-auth/react';
 
 export default function AuthCheck({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
 
-  console.log('AUTH CHECK', session, status);
+  console.log(session, status);
 
   if (status === 'authenticated') {
     return <>{children}</>;
   } else {
-    return <>Not logged in to see this</>;
+    return <></>;
   }
 }
